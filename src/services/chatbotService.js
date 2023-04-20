897c2be4428f3ab1d705d8035907ca766e390bd5
Luan <xoaygio2001@gmail.com>
2023-04-20 13:09:06 +0700

    update

--- src/services/chatbotService.js
+++ src/services/chatbotService.js
@@ -1,6 +1,8 @@
 import request from "request";
 require('dotenv').config();
 
+const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
+
 let callSendAPI = () => {
     let request_body = {
         "recipient": {
@@ -22,8 +24,31 @@ let callSendAPI = () => {
         } else {
             console.log('Unable to send message: ' + err)
         }
-    }
-    )
+    })
+}
+
+let getUserName = (sender_psid) => {
+
+    let userName = '';
+
+    request({
+        "uri": `https://graph.facebook.com/${sender_psid}?fields=first_name,last_name,profile_pic&access_token=${PAGE_ACCESS_TOKEN}`,
+        "qs": { "access_token": PAGE_ACCESS_TOKEN },
+        "method": "POST",
+    }, (err, res, body) => {
+        console.log(body)
+        if (!err) {
+            let response = JSON.parse(res);
+
+            userName = `${response.first_name} ${response.last_name}`;
+
+            console.log('message sent')
+        } else {
+            console.log('Unable to send message: ' + err)
+        }
+    })
+
+    return userName;
 }
 
 let handleMessage = (sender_psid, received_message) => {
