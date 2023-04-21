4b9d2eb5e4859c26053af7e96db79679ad84d298
Luan <xoaygio2001@gmail.com>
2023-04-21 11:31:02 +0700

    update

--- src/services/chatbotService.js
+++ src/services/chatbotService.js
@@ -5,6 +5,10 @@ const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 
 const GUILE_BOOK_DOCTOR = ['http://surl.li/gmwdf', 'http://surl.li/gmwdq'];
 
+const IMG_HOW_ABOUT_BOOKINGCARE = 'http://surl.li/gneut';
+
+const IMG_HOW_ABOUT_DOCTOR = 'http://surl.li/gneve';
+
 let callSendAPI = (sender_psid, response) => {
     let request_body = {
         "recipient": {
@@ -66,41 +70,83 @@ let handleGetStarted = (sender_psid) => {
             let response1 = { "text": `Xin chào ${userName}. Bạn cần hỗ trợ gì!` };
 
             let response2 = {
-                "attachment":{
-                    "type":"template",
-                    "payload":{
-                      "template_type":"generic",
-                      "elements":[
-                         {
-                          "title":"Welcome!",
-                          "image_url":"https://raw.githubusercontent.com/fbsamples/original-coast-clothing/main/public/styles/male-work.jpg",
-                          "subtitle":"We have the right hat for everyone.",
-                          "default_action": {
-                            "type": "web_url",
-                            "url": "https://www.originalcoastclothing.com/",
-                            "webview_height_ratio": "tall"
-                          },
-                          "buttons":[
+                "attachment": {
+                    "type": "template",
+                    "payload": {
+                        "template_type": "generic",
+                        "elements": [
                             {
-                              "type":"web_url",
-                              "url":"https://www.originalcoastclothing.com/",
-                              "title":"View Website"
-                            },{
-                              "type":"postback",
-                              "title":"Start Chatting",
-                              "payload":"DEVELOPER_DEFINED_PAYLOAD"
-                            }              
-                          ]      
-                        }
-                      ]
+                                "title": "Welcome!",
+                                "image_url": "https://raw.githubusercontent.com/fbsamples/original-coast-clothing/main/public/styles/male-work.jpg",
+                                "subtitle": "We have the right hat for everyone.",
+                                "default_action": {
+                                    "type": "web_url",
+                                    "url": "https://www.originalcoastclothing.com/",
+                                    "webview_height_ratio": "tall"
+                                },
+                                "buttons": [
+                                    {
+                                        "type": "web_url",
+                                        "url": "https://www.originalcoastclothing.com/",
+                                        "title": "View Website"
+                                    }, {
+                                        "type": "postback",
+                                        "title": "Start Chatting",
+                                        "payload": "DEVELOPER_DEFINED_PAYLOAD"
+                                    }
+                                ]
+                            },
+                            {
+                                "title": "Welcome!",
+                                "image_url": "https://raw.githubusercontent.com/fbsamples/original-coast-clothing/main/public/styles/male-work.jpg",
+                                "subtitle": "We have the right hat for everyone.",
+                                "default_action": {
+                                    "type": "web_url",
+                                    "url": "https://www.originalcoastclothing.com/",
+                                    "webview_height_ratio": "tall"
+                                },
+                                "buttons": [
+                                    {
+                                        "type": "web_url",
+                                        "url": "https://www.originalcoastclothing.com/",
+                                        "title": "View Website"
+                                    }, {
+                                        "type": "postback",
+                                        "title": "Start Chatting",
+                                        "payload": "DEVELOPER_DEFINED_PAYLOAD"
+                                    }
+                                ]
+                            },
+                            {
+                                "title": "Welcome!",
+                                "image_url": "https://raw.githubusercontent.com/fbsamples/original-coast-clothing/main/public/styles/male-work.jpg",
+                                "subtitle": "We have the right hat for everyone.",
+                                "default_action": {
+                                    "type": "web_url",
+                                    "url": "https://www.originalcoastclothing.com/",
+                                    "webview_height_ratio": "tall"
+                                },
+                                "buttons": [
+                                    {
+                                        "type": "web_url",
+                                        "url": "https://www.originalcoastclothing.com/",
+                                        "title": "View Website"
+                                    }, {
+                                        "type": "postback",
+                                        "title": "Start Chatting",
+                                        "payload": "DEVELOPER_DEFINED_PAYLOAD"
+                                    }
+                                ]
+                            }
+                        ]
                     }
-                  }
+                }
             }
 
             await callSendAPI(sender_psid, response1);
             await callSendAPI(sender_psid, response2);
 
-            
+
 
             resolve("done")
 
