29f69b06ea78b820e782ba3fba050f23251e51c9
xoaygio2001 <xoaygio2001@gmail.com>
2023-09-27 11:33:26 +0700

    update

--- src/services/chatbotService.js
+++ src/services/chatbotService.js
@@ -1,9 +1,12 @@
 import request from "request";
+
+
+
 require('dotenv').config();
 
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 
-const IMG_PROCESS_BOOKING_APPOINTMENT = ['http://surl.li/gmwdf', 'http://surl.li/gmwdq'];
+const IMG_PROCESS_BOOKING_APPOINTMENT = ['../public/a1.png', 'http://surl.li/gmwdq'];
 const IMG_COST_BOOKING_APPOINTMENT = ['http://surl.li/gmwdf', 'http://surl.li/gmwdq'];
 const IMG_CANCEL_APPOINTMENT = ['http://surl.li/gmwdf', 'http://surl.li/gmwdq'];
 const IMG_REALIABLE_DOCTOR = ['http://surl.li/gmwdf', 'http://surl.li/gmwdq'];
